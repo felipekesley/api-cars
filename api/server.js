dc7cbb588a323e5662f48d2e547b7a0e6fcf60ac
Felipe Kesley <fkesley.dev@gmail.com>
2023-07-04 15:05:23 -0300

    first commit

---
+++ api/server.js
@@ -0,0 +1,33 @@
+// See https://github.com/typicode/json-server#module
+const jsonServer = require("json-server");
+const auth = require("json-server-auth");
+const server = jsonServer.create();
+const router = jsonServer.router("db.json");
+//const middlewares = jsonServer.defaults();
+
+// server.use(middlewares);
+// // Add this before server.use(router)
+// server.use(
+//   jsonServer.rewriter({
+//     "/api/*": "/$1",
+//     "/blog/:resource/:id/show": "/:resource/:id",
+//   })
+// );
+
+const rules = auth.rewriter({
+  // Permission rules
+  users: 600,
+  messages: 640,
+  // Other rules
+  "/posts/:category": "/posts?category=:category",
+});
+
+server.use(rules);
+server.use(auth);
+server.use(router);
+server.listen(3000, () => {
+  console.log("JSON Server is running");
+});
+
+// Export the Server API
+module.exports = server;
